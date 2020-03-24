a0418fa24dd064277e7b839f74413ce80fdef86f
raavan95 <46168605+raavan95@users.noreply.github.com>
2020-03-24 15:28:13 +0530

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -1,5 +1,5 @@
 import {withRouter} from 'next/router'
-const Post=withRouter((props)=>{
+const Post=withRouter(props)=>{
     return (
         <h1>{props.router.query.title}</h1>
         <p>This is dynamic content</p>
